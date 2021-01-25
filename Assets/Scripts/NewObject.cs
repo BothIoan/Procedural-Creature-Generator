c251deb1f4c494ec5e975bd3ccd75fb556ce7781
ionic <ionic@DESKTOP-9GN3T2B>
2021-01-25 15:47:49 +0200

    part of code cleanup

--- Assets/Scripts/NewObject.cs
+++ Assets/Scripts/NewObject.cs
@@ -3,10 +3,18 @@ using UnityEngine.UI;
 using System.Collections;
 using System.Collections.Generic;
 
-// rename this class to suit your needs
 public class NewObject : MonoBehaviour
 {
-   public struct symPair
+
+    // the Equip prefab - required for instantiation
+    public GameObject equipPrefab;
+    public GameObject parent;
+    public List<GameObject> createdObjects = new List<GameObject>();
+    private float minX, maxX, minY, maxY;
+    public GameObject currentSymPlane;
+    int currentDSign;
+
+    public struct symPair
     {
         public Transform transform1;
         public Transform transform2;
@@ -18,26 +26,7 @@ public class NewObject : MonoBehaviour
         }
     }
 
-    // the Equip prefab - required for instantiation
-    public GameObject equipPrefab;
-    public GameObject parent;
-    // list that holds all created objects - deleate all instances if desired
-    public List<GameObject> createdObjects = new List<GameObject>();
-    private float minX, maxX, minY, maxY;
-
-    void Start()
-    {
-        // get the screen bounds
-        float camDistance = Vector3.Distance(transform.position, Camera.main.transform.position);
-        Vector2 bottomCorner = Camera.main.ViewportToWorldPoint(new Vector3(0, 0, camDistance));
-        Vector2 topCorner = Camera.main.ViewportToWorldPoint(new Vector3(1, 1, camDistance));
-
-        minX = bottomCorner.x;
-        maxX = topCorner.x;
-        minY = bottomCorner.y;
-        maxY = topCorner.y;
-    }
-    public void LegsStage()
+    public void Cleanup()
     {
         //temporary, to see different results;
         createdObjects.ForEach((x) =>
@@ -46,50 +35,70 @@ public class NewObject : MonoBehaviour
         });
         createdObjects.Clear();
         //temp
+    }
 
-        //plane stage
-        GameObject symPlane = symPlaneCreate();
-        maxX = symPlane.GetComponent<MeshRenderer>().bounds.size.x;
+   
 
-        //direction is used for the legs.
-        float direction = maxX / 2 + symPlane.transform.position.x;
-        Vector3 test = symPlane.transform.position;
-        test.x = direction;
-        //
+    public GameObject PlaneStage()
+    {
+        Vector3 initial = new Vector3(501, 5, 510);
+        GameObject symPlane = (GameObject)Instantiate(parent, initial, Quaternion.identity);
+        createdObjects.Add(symPlane);
+        currentSymPlane = symPlane;
+        return symPlane;
+    }
+
+    public void getDirection()
+    {
+        currentDSign = -1;
+        maxX = currentSymPlane.GetComponent<MeshRenderer>().bounds.size.x;
+        float currentDirection = maxX / 2 + currentSymPlane.transform.position.x;
+        if (currentDirection > currentSymPlane.transform.position.x)
+        {
+            currentDSign = 1;
+        }
+    }
+
+    public void LegsStage()
+    {
         int nrOfLegs = Random.Range(1,5);
-        Vector3 legAttPos = symPlane.transform.position;
+        Vector3 legAttPos = currentSymPlane.transform.position;
         float distanceZ = Random.Range(0f, 3f);
-        float directionSign;
-        if(direction> symPlane.transform.position.x)
-        {
-            directionSign = 1;
-        }
-        else
-        {
-            directionSign = -1;
-        }
+        
         float ammount = 0;
         for (int i = 0; i < nrOfLegs; i++)
         {
-            
-            
-            legAttPos.x = symPlane.transform.position.x + ammount;
+            legAttPos.x = currentSymPlane.transform.position.x + ammount;
             GameObject legAtt = (GameObject)Instantiate(equipPrefab, legAttPos, Quaternion.identity);
-            legAtt.transform.parent = symPlane.transform;
+            legAtt.transform.parent = currentSymPlane.transform;
             createdObjects.Add(legAtt);
             
             float distanceX = -1*(ammount + Random.Range(1f,1.5f));
 
-            symPair joints = MirrorCreate(symPlane.transform,legAtt.transform,legAtt.transform, distanceX, 3, distanceZ);
-            MirrorCreate(symPlane.transform,joints.transform1,joints.transform2, (distanceX + 1) * directionSign, 4, distanceZ);
+            symPair joints = MirrorCreate(currentSymPlane.transform,legAtt.transform,legAtt.transform, distanceX, 3, distanceZ);
+            MirrorCreate(currentSymPlane.transform,joints.transform1,joints.transform2, (distanceX + 1) * currentDSign, 4, distanceZ);
             if (i == 0)
             {
-                SpineStage(legAtt.transform,symPlane.transform);
+                SpineStage(legAtt.transform,currentSymPlane.transform);
             }
             ammount += Random.Range(-2f, -3f);
         }
     }
 
+    public void SpineStage(Transform parent, Transform symPlane)
+    {
+        Vector3 headPos = parent.position;
+        headPos.y += Random.Range(3f, 5f);
+        GameObject head = (GameObject)Instantiate(equipPrefab, headPos, Quaternion.identity);
+        head.transform.parent = parent;
+        createdObjects.Add(head);
+        MirrorCreate(symPlane, head.transform, head.transform, 0, Random.Range(0.5f, 1f), Random.Range(2f, 3f));
+    }
+
+    public void ArmsStage()
+    {
+
+    }
 
     public symPair MirrorCreate(Transform symPlanT, Transform parent1T, Transform parent2T, float distanceX, float distanceY, float distanceZ)
     {
@@ -111,30 +120,11 @@ public class NewObject : MonoBehaviour
         return new symPair(g1.transform, g2.transform);
     }
 
-    public GameObject symPlaneCreate()
+    public void pressButton()
     {
-        if (equipPrefab != null)
-        {
-            Vector3 initial = new Vector3(501, 5, 510);
-            GameObject symPlane = (GameObject)Instantiate(parent, initial, Quaternion.identity);
-            createdObjects.Add(symPlane);
-            return symPlane;
-        }
-        return null;
-    }
-
-    public void SpineStage(Transform parent,Transform symPlane)
-    {
-        Vector3 headPos = parent.position;
-        headPos.y += Random.Range(3f,5f);
-        GameObject head = (GameObject)Instantiate(equipPrefab, headPos, Quaternion.identity);
-        head.transform.parent = parent;
-        createdObjects.Add(head);
-        MirrorCreate(symPlane, head.transform, head.transform, 0, Random.Range(0.5f, 1f), Random.Range(2f, 3f));
-    }
-
-    public void ArmsStage()
-    {
-
+        Cleanup();
+        PlaneStage();
+        getDirection();
+        LegsStage();
     }
 }
